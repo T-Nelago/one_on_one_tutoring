3095ded95cee5a2f908f83e063aef436a292599e
Vernon Swanepoel <frantic.naturalist@gmail.com>
2018-11-16 05:35:18 +0200

    Created first program to run.

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Hello World!
+#
+# This is going to be very easy instructions, just to get us started.
+#
+# The instructions here are simple.
+# - Start a line of code below
+# - Type the word 'print'
+# - After print, type an opening bracket - this one '('
+# - After the opening bracket, type a '"'
+# - Now type 'Hello World!' after the '"'
+# - After that, close the '"'
+# - Now, finally a closing bracket.
+# - Save the program
+# - run it at the command line with $ python hello.py - note, this is also the first move in testing your code.
+# - If it doesn't work, first try to see if you can figure out where you need to fix something.
+# - When it works, add it to git and update your git repo.  Your git commit might say "completed Hello World program"
